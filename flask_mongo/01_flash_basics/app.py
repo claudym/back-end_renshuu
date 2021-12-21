3fc23fb2a9e6e94e1e0b6f29278577ef7bfd259d
Kura <kura@localhost.localdomain>
2021-12-21 14:47:17 -0400

    flask basics and simple api creation

---
+++ flask_mongo/01_flash_basics/app.py
@@ -0,0 +1,88 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+#127.0.0.1:5000/
+@app.route('/')
+def hello_world():
+  return "Hello 世界！"
+
+@app.route('/hithere')
+def hi_there_minna():
+  return "/hithere hitto!"
+
+@app.route('/bye')
+def bye():
+  #prepare a response for the request that came to /bye
+  # joyo = "jouyou kanji yo"
+
+  # tempJson={
+  #   "field1":123
+  # }
+
+  # retJson= {
+  #   "field1": 3,
+  #   "field2": "def",
+  #   "boolean": True,
+  #   "array": [1,2,3,4,"abc"],
+  #   "array of objects": [
+  #     {
+  #       "field1": 1
+  #     },
+  #     {
+  #       "field2": "string yo!"
+  #     }
+  #   ],
+  #   "array of nested arrays": [
+  #     {
+  #       "nested array": [
+  #         {
+  #           "field1": 1,
+  #           "name": "kura"
+  #         }
+  #       ]
+  #     }
+  #   ]
+  # }
+  age = 2 * 14
+  retJson = {
+    "name": "kura",
+    "age": age,
+    "phones": [
+      {
+        "phoneName": "LG 200",
+        "phoneNumber": 394725233,
+        "wifi": False
+      },
+      {
+        "phoneName": "iphone12",
+        "phoneNumber": 44444,
+        "wifi": True
+      }
+    ]
+  }
+  return retJson
+
+@app.route("/add", methods=["POST"])
+def add_two_nums():
+  #get x,y from posted data
+  dataDict = request.get_json()
+
+  if "y" not in dataDict:
+    return "ERROR", 305
+  x= dataDict["x"]
+  y= dataDict["y"]
+
+  #add z=x+y
+  z= x+y
+
+  #prepare JSON, "z":z
+  retJson = {
+    "z": z
+  }
+
+  #return map_prepared
+  return retJson
+
+if __name__ == "__main__":
+  # app.run(host="127.0.0.1", port=80)
+  app.run(debug=True)
---
+++ flask_mongo/02_simple_api/app.py
@@ -0,0 +1,135 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+def checkPostedData(postedData, functionName):
+  if functionName in ["add", "subtract", "multiply"]:
+    if "x" not in postedData or "y" not in postedData:
+      return 301
+    elif not isinstance(postedData["x"], int) or not isinstance(postedData["y"], int):
+      return 302
+    else:
+      return 200
+  elif functionName == "divide":
+    if "x" not in postedData or "y" not in postedData:
+      return 301
+    elif not isinstance(postedData["x"], int) or not isinstance(postedData["y"], int):
+      return 302
+    elif postedData["y"] == 0:
+      return 303
+    else:
+      return 200
+
+class Add(Resource):
+  def post(self):
+    postedData = request.get_json()
+
+    # verify postedData
+    status_code = checkPostedData(postedData, "add")
+    if status_code != 200:
+      retJson = {
+        "message": "Error!",
+        "status code": status_code
+      }
+      return retJson
+
+    x = postedData["x"]
+    y = postedData["y"]
+    x = int(x)
+    y = int(y)
+    ret= x+y
+    retMap = {
+      "message": ret,
+      "status code": 200
+    }
+
+    return retMap
+
+class Subtract(Resource):
+  def post(self):
+    postedData = request.get_json()
+
+    # verify postedData
+    status_code = checkPostedData(postedData, "subtract")
+    if status_code != 200:
+      retJson = {
+        "message": "Error!",
+        "status code": status_code
+      }
+      return retJson
+
+    x = postedData["x"]
+    y = postedData["y"]
+    x = int(x)
+    y = int(y)
+    ret= x-y
+    retMap = {
+      "message": ret,
+      "status code": 200
+    }
+
+    return retMap
+
+class Multiply(Resource):
+  def post(self):
+    postedData = request.get_json()
+
+    # verify postedData
+    status_code = checkPostedData(postedData, "multiply")
+    if status_code != 200:
+      retJson = {
+        "message": "Error!",
+        "status code": status_code
+      }
+      return retJson
+
+    x = postedData["x"]
+    y = postedData["y"]
+    x = int(x)
+    y = int(y)
+    ret= x*y
+    retMap = {
+      "message": ret,
+      "status code": 200
+    }
+
+    return retMap
+
+class Divide(Resource):
+  def post(self):
+    postedData = request.get_json()
+
+    # verify postedData
+    status_code = checkPostedData(postedData, "divide")
+    if status_code != 200:
+      retJson = {
+        "message": "Error!",
+        "status code": status_code
+      }
+      return retJson
+
+    x = postedData["x"]
+    y = postedData["y"]
+    x = int(x)
+    y = int(y)
+    ret= x/y
+    retMap = {
+      "message": ret,
+      "status code": 200
+    }
+
+    return retMap
+
+api.add_resource(Add, "/add")
+api.add_resource(Subtract, "/subtract")
+api.add_resource(Multiply, "/multiply")
+api.add_resource(Divide, "/divide")
+
+@app.route("/")
+def hello():
+  return "hola"
+
+if __name__ == "__main__":
+  app.run(debug = True)
