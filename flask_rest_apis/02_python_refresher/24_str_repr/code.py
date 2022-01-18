a95413fdf8b4846e173a95c1ea525f90ff16eaab
claudym <claudy.m.mejia@gmail.com>
2022-01-17 22:10:05 -0400

    sqlalchemized

--- flask_rest_apis/02_python_refresher/24_str_repr/code.py
+++ flask_rest_apis/02_python_refresher/24_str_repr/code.py
@@ -1,15 +1,15 @@
-# class Person:
-#   def __init__(self, name, age):
-#     self.name= name
-#     self.age= age
-#   def __str__(self):
-#     return f"Person {self.name}, {self.age} years old."
-#   def __repr__(self):
-#     return f"<Person('{self.name}', {self.age})>"
+class Person:
+  def __init__(self, name, age):
+    self.name= name
+    self.age= age
+  def __str__(self):
+    return f"Person {self.name}, {self.age} years old."
+  def __repr__(self):
+    return f"<Person('{self.name}', {self.age})>"
 
-# bobby= Person("Bobby", 24)
-# print(bobby)
-# print(bobby.__repr__())
+bobby= Person("Bobby", 24)
+print(bobby)
+print(bobby.__repr__())
 
 
 class Store:
