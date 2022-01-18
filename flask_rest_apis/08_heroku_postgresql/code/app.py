d1430e019ebf376d46e92d0791228d7380626f8c
claudym <claudy.m.mejia@gmail.com>
2022-01-18 16:46:23 -0400

    using postgresql and heroku hosting

---
+++ flask_rest_apis/08_heroku_postgresql/code/app.py
@@ -0,0 +1,33 @@
+import os
+
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+db_url= os.environ.get("DATABASE_URL", "sqlite:///data.db")
+if db_url != "sqlite:///data.db":
+  if db_url[:10] != "postgresql":
+    db_url= f"postgresql{db_url[8:]}"
+app.config['SQLALCHEMY_DATABASE_URI']= db_url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'el secreto'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+  db.init_app(app)
+  app.run(debug=True)  # important to mention debug=True
